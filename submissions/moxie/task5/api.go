f0330093dc8fe6aa9431b617311feb128b001cd1
profraj1 <35667126+profraj1@users.noreply.github.com>
2018-03-17 13:06:15 +0530

    Create api.go

---
+++ submissions/moxie/task5/api.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+func main() {
+	url := "http://www.facebook.com"
+	fmt.Printf("HTML code of %s ...\n", url)
+	resp, err := http.Get(url)
+	// handle the error if there is one
+	if err != nil {
+		panic(err)
+	}
+	// do this now so it won't be forgotten
+	defer resp.Body.Close()
+	// reads html as a slice of bytes
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	// show the HTML code as a string %s
+	fmt.Printf("%s\n", html)
+}
